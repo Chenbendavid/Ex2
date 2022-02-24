f13fbdc1b9fc9cfcce1ad0cf12dd0d186450fc69
chen ben-david <chen.ben.david@centrical.com>
2022-02-24 12:23:34 +0200

    lesson 15

---
+++ Arrays/ExArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2.Arrays
+{
+    internal class ExArray
+    {
+        public void StringWords()
+        {
+            string[] words = { "Csharp", "is", "a", "general", "purpose", "computer", "programming", "language" };
+            Console.WriteLine(words[5]);
+            Console.WriteLine(words[6]);
+            Console.WriteLine(words[7]);
+            Console.ReadKey();
+        }
+
+        public void StringWords2()
+        {
+            string words = "Csharp,is,a,general,purpose,computer,programming,language,love";
+            string[] words2 = words.Split(',');
+            Console.WriteLine(words2[words2.Length - 3]);
+            Console.WriteLine(words2[words2.Length - 2]);
+            Console.WriteLine(words2[words2.Length - 1]);
+            Console.ReadKey();
+        }
+
+        public void PeapleAndPlaces()
+        {
+            string[,] arrayPeapleAndPlaces = { { "Moshe", "22", "Jerusalem"} , { "Joseph", "45", "London"} };
+            Console.WriteLine(arrayPeapleAndPlaces[0,0]);
+            Console.ReadKey();
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ex2.Arrays;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,15 +9,29 @@ namespace Ex2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+            //Sefi();
+            //Chen();
+            Female();
+            //ExArray exArray = new ExArray();
+            //exArray.StringWords();
+            //exArray.StringWords2();
+            //exArray.PeapleAndPlaces();  
+
+        }
+
+        public static void Sefi()
         {
             Console.WriteLine("Hello \r\nWorld:)");
             Console.WriteLine();
             Console.WriteLine("LOVE IS IN THE AIR!");
-            string first_name = "Chen";
-            string last_name = "Ben-David";
-            String full_name = String.Concat(first_name, " ", last_name);
-            Console.WriteLine(full_name);
+            string firstName = "Chen";
+            string lastName = "Ben-David";
+            String fullName = String.Concat(firstName, " ", lastName);
+            Console.WriteLine(fullName);
 
             Console.WriteLine("*****************************************************");
 
@@ -26,56 +41,57 @@ namespace Ex2
             Console.WriteLine(title.ToUpper());
 
             Console.Write("Insert your birth year > ");
-            int birth_year = int.Parse(Console.ReadLine());
-            int age = 2022 - birth_year;
-            Console.WriteLine("your age is: "+age);
+            int birthYear = int.Parse(Console.ReadLine());
+            int age = 2022 - birthYear;
+            Console.WriteLine("your age is: " + age);
             Console.WriteLine("*****************************************************");
+        }
 
+        public static void Chen()
+        {
             Console.Write("What's your name? ");
             string name = Console.ReadLine();
             Console.WriteLine("Your first name char is: " + name.First());
             Console.WriteLine("*****************************************************");
 
             Console.Write("Enter your Math grade > ");
-            double Math_grade = double.Parse(Console.ReadLine());
+            double mathGrade = double.Parse(Console.ReadLine());
             Console.Write("Enter your Hebrow grade > ");
-            double Hebrow_grade = double.Parse(Console.ReadLine());
+            double hebrowGrade = double.Parse(Console.ReadLine());
             Console.Write("Enter your English grade > ");
-            double English_grade = double.Parse(Console.ReadLine());
-            double Average = (Math_grade + Hebrow_grade + English_grade)/3;
-            int rounded_average = (int)Average;
-            Console.WriteLine("Your schoole average is: " + Average +", But you can round it to: " + rounded_average);
+            double englishGrade = double.Parse(Console.ReadLine());
+            double average = (mathGrade + hebrowGrade + englishGrade) / 3;
+            int rounded_average = (int)average;
+            Console.WriteLine("Your schoole average is: " + average + ", But you can round it to: " + rounded_average);
             Console.WriteLine("*****************************************************");
+        }
 
+        public static void Female()
+        {
             string firstN = "chen";
             string lastN = "Ben-David";
-            int Age_chen = 31;
-            Age_chen.ToString();
-            Console.WriteLine(Age_chen);
+            var ageChen = 31;
+            ageChen.ToString();
+            Console.WriteLine(ageChen);
             string fullN = (firstN + lastN);
             Console.WriteLine(fullN.ToUpper() + (fullN.Count()));
-            bool result = (fullN.Count()).Equals(Age_chen);
+            bool result = (fullN.Count()).Equals(ageChen);
             Console.WriteLine(result);
             Console.WriteLine("*****************************************************");
 
 
             Console.Write("Enter your gender (Write F for female or M for male > ");
-            string female_gender = Console.ReadLine();
-            Boolean Gender = string.Equals(female_gender,"F", StringComparison.OrdinalIgnoreCase);
-            if (Gender == false)
+            string femaleGender = Console.ReadLine();
+            Boolean gender = string.Equals(femaleGender, "F", StringComparison.OrdinalIgnoreCase);
+            if (gender == false)
             {
                 Console.WriteLine("MAN!! go away!");
             }
             else
                 Console.WriteLine("You are a strong women! :)");
-                   
-                   
-                   
-
-            Console.ReadKey();  
-
+            Console.ReadKey();
         }
 
-    
     }
 }
+
